a7f9d913493323943619754c79ae09cd775b017a
mrcharleske <njorogecharles5460@gmail.com>
2025-09-22 15:47:43 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,7 @@ const urlsToCache = [
     '/contact.html',
     '/styles.css',
     '/main.js',
+    '/site.webmanifest',
     '/images/logo.png',
     '/images/livestreaming.jpg',
     '/images/branding.jpg',
@@ -46,13 +47,41 @@ const urlsToCache = [
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+            .catch(error => {
+                console.error('Cache addAll failed:', error);
+            })
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).catch(() => {
+                    return caches.match('/index.html');
+                });
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
